3565221a3f43eb74a4ebdba53542f623a01fb075
LostAbaddon <lostabaddon@foxmail.com>
2020-02-13 13:02:12 +0800

    '增加快捷键：Ctrl+M'

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
-chrome.browserAction.onClicked.addListener(tab => {
-	chrome.tabs.sendMessage(tab.id, {
+const ToggleMosaic = tabID => {
+	chrome.tabs.sendMessage(tabID, {
 		action: "launch",
 		level: 1
 	});
+};
+
+chrome.browserAction.onClicked.addListener(tab => {
+	ToggleMosaic(tab.id);
+});
+
+chrome.commands.onCommand.addListener(cmd => {
+	if (cmd === 'toggle-mosaic') {
+		chrome.tabs.getSelected(tab => {
+			ToggleMosaic(tab.id);
+		});
+	}
 });
